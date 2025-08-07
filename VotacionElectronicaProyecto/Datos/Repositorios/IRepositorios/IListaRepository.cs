b0461e950089f7156f136cfa56343e281d029d13
Trinidad <trinidad.bonzi.ices@gmail.com>
2025-08-07 10:22:18 -0300

    cambiosenlista

---
+++ VotacionElectronicaProyecto/Datos/Repositorios/IRepositorios/IListaRepository.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datos.Repositorios.IRepositorios
+{
+    public interface IListaRepository
+    {
+        Task<List<Lista>> ObtenerTodos();
+        Task<Lista> ObtenerPorId(int id);
+        Task<List<Lista>> BuscarPorNombre(string nombre);
+        Task<Lista> Crear(Lista lista);
+        Task Actualizar(Lista lista);
+        Task Eliminar(int id);
+    }
+}
---
+++ VotacionElectronicaProyecto/Negocio/Logica/ILogica/IListaLogic.cs
@@ -0,0 +1,19 @@
+﻿using Shared.Dtos.Lista;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Logica.ILogica
+{
+    public interface IListaLogic
+    {
+        Task<List<VerDTO>> ObtenerListas();
+        Task<VerDTO> ObtenerListasPorId(int id);
+        Task<List<VerDTO>> ObtenerListasPorNombre(string nombre);
+        Task CrearLista(CrearDTO dto);
+        Task ActualizarLista(int id, ModificarDTO dto);
+        Task EliminarLista(int id);
+    }
+}
