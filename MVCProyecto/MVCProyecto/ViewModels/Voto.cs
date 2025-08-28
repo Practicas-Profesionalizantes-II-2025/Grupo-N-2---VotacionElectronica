3bac09c209b450c48cc84d10c19295bfabbdb238
Taco <taco91218@gmail.com>
2025-08-28 09:49:45 -0300

    Controller Candidatos

---
+++ MVCProyecto/MVCProyecto/ViewModels/Voto.cs
@@ -0,0 +1,16 @@
+﻿namespace MVCProyecto.ViewModels
+{
+    public class Voto
+    {
+        public DateTime FechaVoto { get; set; }
+        public DateTime HoraVoto { get; set; }
+
+        public int EleccionId { get; set; }
+        public Eleccion Eleccion { get; set; }
+
+        public int ListaId { get; set; }
+        public Lista Lista { get; set; }
+        public int? ResultadoId { get; set; } // Hacerlo nullable
+        public Resultado Resultado { get; set; }
+    }
+}
