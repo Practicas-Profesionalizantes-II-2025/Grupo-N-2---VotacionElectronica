e2871e9d3ce5eff5f30cb3b3f30995347f9e65c5
Trinidad <trinidad.bonzi.ices@gmail.com>
2025-09-11 18:56:21 -0300

    botonsaliragregado

--- MVCProyecto/MVCProyecto/obj/Debug/net8.0/MVCProyecto.AssemblyInfo.cs
+++ MVCProyecto/MVCProyecto/obj/Debug/net8.0/MVCProyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5396287de658afdbf28144bf5b62b9b938a97365")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e2cc826831c693df3c9b2e1da0a627cbdd7e55e")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
