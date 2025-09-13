890076a6898e036926da120e31371fc7683d958f
Trinidad <trinidad.bonzi.ices@gmail.com>
2025-09-13 14:59:04 -0300

    cambiosenlogin

--- MVCProyecto/MVCProyecto/obj/Debug/net8.0/MVCProyecto.AssemblyInfo.cs
+++ MVCProyecto/MVCProyecto/obj/Debug/net8.0/MVCProyecto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MVCProyecto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c080bfacd200c8d253eb60b07f14c024638cfb9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7c5330a43a1d30461a41c9b4aae2acc1c237d2b")]
 [assembly: System.Reflection.AssemblyProductAttribute("MVCProyecto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MVCProyecto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
