dd56a7f9a28e3ecfec1f63192dec02e5986a0545
Taco <taco91218@gmail.com>
2025-10-14 13:25:39 -0300

    DTO

---
+++ MVCProyecto/MVCProyecto/Models/Eleccion/VerDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCProyecto.Models.Eleccion
+{
+    public class VerDTO
+    {
+        public int Id { get; set; }
+        public string NombreEleccion { get; set; }
+        public string DescripcionEleccion { get; set; }
+        public int CantidadListas { get; set; }
+        public DateTime? FechaInicioEleccion { get; set; }
+        public DateTime? FechaFinEleccion { get; set; }
+        public DateTime CreatedDate { get; set; }  
+        public DateTime UpdatedDate { get; set; } 
+
+        // public List<ListaDTO> Listas { get; set; }
+
+    }
+}
